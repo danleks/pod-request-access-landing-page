c4c30fe52f5d3ee303345b476a2c76df227f34d7
danleks <danilevich.lks@gmail.com>
2021-03-21 23:34:30 +0100

    Create Input

---
+++ src/components/Input/Input.js
@@ -0,0 +1,11 @@
+import './Input.styles.sass';
+
+const Input = (() => {
+    let html = `
+        <input type="email" placeholder="Email address" />
+    `;
+
+    return html;
+})();
+
+export default Input;
