87eb4288305040e2100fe5026b3950dc097737ed
danleks <danilevich.lks@gmail.com>
2021-03-21 23:35:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import './styles/style.sass';
+import './styles/main.sass';
+import Header from './components/Header/Header';
+import Dots from './components/Dots/Dots';
+import CallToAction from './components/CallToAction/CallToAction';
+
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+    <div class="mainWrapper">
+        <main>
+            ${Header}
+            ${CallToAction}
+            ${Dots}
+        </main>
+    </div>
+`;
