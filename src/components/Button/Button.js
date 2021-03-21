f85591ae8228d8c7bb7bf50e498d04d1197e2962
danleks <danilevich.lks@gmail.com>
2021-03-21 23:32:54 +0100

    Create Button.js

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import './Button.styles.sass';
+import dots from '../../assets/desktop/bg-pattern-dots.svg';
+
+const Button = (() => {
+    let html = `
+       <button class="button" type="submit">Request Access</button>
+    `;
+
+    return html;
+})();
+
+export default Button;
