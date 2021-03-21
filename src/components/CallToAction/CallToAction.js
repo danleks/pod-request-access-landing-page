ee5e34c30f7720ac87a2e5947fc1dcf2b23b26ef
danleks <danilevich.lks@gmail.com>
2021-03-21 23:33:19 +0100

    Create callToAction

---
+++ src/components/CallToAction/CallToAction.js
@@ -0,0 +1,52 @@
+import './CallToAction.styles.sass';
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+import {validateEmail} from '../../utils/validateEmail';
+import applePodcast from '../../assets/desktop/apple-podcast.svg';
+import googlePodcast from '../../assets/desktop/google-podcasts.svg';
+import poceketCast from '../../assets/desktop/pocket-casts.svg';
+import spotify from '../../assets/desktop/spotify.svg';
+
+const icons = [
+    {   
+        id: 0,
+        name: 'spotify',
+        src: spotify
+    },
+    {   
+        id: 1,
+        name: 'apple-podcast',
+        src: applePodcast
+    },
+    {   
+        id: 2,
+        name: 'google-podcast',
+        src: googlePodcast
+    },
+    {   
+        id: 3,
+        name: 'pocket-cast',
+        src: poceketCast
+    },   
+];
+
+const CallToAction = (() => {
+    let html = `
+        <div class="callToAction">
+            <h1>Publish your podcasts<span class="highlight">everywhere.</span></h1>
+            <p>Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!</p>
+            <form>
+                ${Input}
+                ${Button}
+            </form>
+            <span class="error">Oops! Please check your email</span>
+            <ul>
+                ${icons.map(icon => `<li><img src=${icon.src} alt=${icon.name}/></li>`).join('')}
+            </ul>
+        </div>
+    `;
+
+    return html;
+})();
+
+export default CallToAction;
