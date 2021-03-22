062ded982d6b79ce8c121dc685d06e1623e48b14
danleks <danilevich.lks@gmail.com>
2021-03-22 22:13:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import './styles/main.sass';
 import Header from './components/Header/Header';
 import Dots from './components/Dots/Dots';
 import CallToAction from './components/CallToAction/CallToAction';
+import Overlay from './components/Overlay/Overlay';
 
 const app = document.querySelector('#app');
 
 app.innerHTML = `
     <div class="mainWrapper">
+        ${Overlay}
         <main>
             ${Header}
             ${CallToAction}
